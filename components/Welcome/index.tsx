9e6ff43eef234721f657b080381b99520246dcbc
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-08 02:40:03 -0700

    scaffolding portfolio, and making components

---
+++ components/Welcome/index.tsx
@@ -0,0 +1,17 @@
+export default function Welcome() {
+  return (
+    <div className='bg-white'>
+      <div className='mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
+        <div className='text-center'>
+          <h2 className='text-lg font-semibold text-sky-800'>Hi, my name is</h2>
+          <p className='mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl'>
+            Dylan Ullrich
+          </p>
+          <p className='mx-auto mt-5 max-w-xl text-xl text-gray-500'>
+            I am a Full Stack Web Developer.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
