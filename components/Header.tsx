86f00e7c24ce42784eac0a557b58785ce194023b
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-09 13:45:48 -0700

    proper type

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
-function classNames(...classes: boolean[]) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
