8a710293854269a0ef6c361783fc3b1f093a3852
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-14 15:05:00 -0700

    change button name

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -119,7 +119,7 @@ export default function Contact() {
                 type='submit'
                 className='inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
               >
-                Let's talk
+                Send
               </button>
             </div>
           </form>
