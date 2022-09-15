55ecc1d877f9e7739f7535f18d2177041745ee6d
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-14 19:26:35 -0700

    add links to github/linkedin

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Intro() {
   return (
     <div className='flex  h-[calc(100vh-72px)] flex-col items-center justify-center'>
@@ -9,6 +12,32 @@ export default function Intro() {
       <p className=' mt-5 max-w-xl text-xl text-gray-500'>
         I am a Full Stack Developer.
       </p>
+      <div className='flex gap-4'>
+        <div className='py-3'>
+          <a className='cursor-pointer'>
+            <Link href='https://github.com/GetUllrichorDieTrying'>
+              <Image
+                src='/images/GitHub-Mark-Light-120px-plus.png'
+                alt='link to github'
+                width={32}
+                height={32}
+              />
+            </Link>
+          </a>
+        </div>
+        <div className='py-3'>
+          <a className='cursor-pointer'>
+            <Link href='https://www.linkedin.com/in/dsullrich/'>
+              <Image
+                src='/images/linkedin.svg'
+                alt='link to linkedin'
+                width={32}
+                height={32}
+              />
+            </Link>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
