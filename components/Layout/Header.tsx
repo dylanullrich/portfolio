fb30cdc6f48cf057a9e5bd9169f9db9992282703
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-11 15:23:37 -0700

    add plausible script

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -13,6 +13,11 @@ export default function Header() {
     <>
       <Head>
         <title>Get Ullrich or Die Trying</title>
+        <script
+          defer
+          data-domain='dylanullrich.com'
+          src='https://plausible.io/js/plausible.js'
+        ></script>
       </Head>
       <header className='bg-slate-800'>
         <nav
