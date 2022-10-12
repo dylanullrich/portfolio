20f375016146e142defaf9652c54f24a117a26b3
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-11 17:45:33 -0700

    add next-plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import PlausibleProvider from 'next-plausible';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <PlausibleProvider domain='dylanullrich.com'>
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   );
 }
 
