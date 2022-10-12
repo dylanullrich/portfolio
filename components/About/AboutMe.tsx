a0e700601f81f20fa2145e915717df4df26bf618
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-12 14:09:42 -0700

    content change

--- components/About/AboutMe.tsx
+++ components/About/AboutMe.tsx
@@ -4,22 +4,21 @@ export default function AboutMe() {
       <div className='h-full divide-y divide-gray-400 overflow-hidden rounded-lg bg-slate-700 shadow'>
         <div className='border-b border-white  px-4 py-5 sm:px-6'>
           <h3 className='text-center text-lg font-medium leading-6 text-white'>
-            A little about me...
+            A little about me
           </h3>
         </div>
-        <div className='p-4 text-gray-400 sm:flex sm:items-center sm:p-6 md:h-3/4'>
-          <p>
-            I am a Veteran with an Active Secret Clearance, and recent
-            experience working in Business Development at a tech startup. While
-            there, I found myself increasingly interested in streamlining and
-            automating processes. I realized that I loved the thinking that went
-            into integrating information from multiple platforms, which lead me
-            back to the world of coding.
+        <div className='p-4 text-gray-300 sm:flex sm:items-center sm:p-6 md:h-3/4'>
+          <ul className='list-disc pl-4'>
+            <li>Army Veteran.</li>
             <br />
-            <br />I earned my Bachelor of Arts in Law, Economics, and Public
-            Policy from University of Washington in March 2022, and since then I
-            have devoted my time to honing my skills as a Software Developer.
-          </p>
+            <li>I love to build things.</li>
+            <br />
+            <li>Open Source Enthusiast.</li>
+            <br />
+            <li>Entrepreneurship on my mind.</li>
+            <br />
+            <li>Always learning.</li>
+          </ul>
         </div>
       </div>
     </div>
