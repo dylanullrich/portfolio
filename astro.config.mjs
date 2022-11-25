9044dd9bbc3c631d50dee4817e7b19fc3eb6d7c9
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-11-24 16:01:21 -0800

    fix semi colon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
 });
