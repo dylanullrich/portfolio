2336a4f9150256ea147af8d3b7f2cbc8e97967b1
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-12-11 13:44:15 -0800

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+// https://astro.build/configz
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel(),
 });
