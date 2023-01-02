66bcc0d9cf6a014279e4a896aa6b99555b86cd32
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2023-01-01 17:36:06 -0800

    incl integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
+import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.dylanullrich.com/",
+  integrations: [tailwind(), sitemap(), robotsTxt()],
 });
