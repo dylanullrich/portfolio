1ad230b9066113c11294be7edca1818176249a0a
DYLAN ULLRICHU <ME@DYLANULLRICH.COM>
2023-10-06 20:57:43 -0700

    added astro icon w/heroicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon({
+    include: {
+      heroicons: ['*'],
+    },
+  })],
 });
