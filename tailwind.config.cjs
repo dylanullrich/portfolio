86a52e2aa17da2114380aad1ae8c537bcc357e73
DYLAN ULLRICHU <ME@DYLANULLRICH.COM>
2023-10-08 17:03:58 -0700

    add text shadow utility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+      sm: '0 1px 2px var(--tw-shadow-color)',
+      DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+      lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'),
+    (function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
