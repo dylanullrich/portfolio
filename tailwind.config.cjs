fec63ee2189256e110604244007ceb8777cc324b
DYLAN ULLRICHU <ME@DYLANULLRICH.COM>
2023-10-12 18:46:22 -0700

    add fade/out to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
-      dropShadow: {
-        custom: '0 5px 5px var(--tw-shadow-color)'
-      }
-    },
-  },
-  plugins: [require('@tailwindcss/typography'),
-    (function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': (value) => ({
-            textShadow: value,
-          }),
+  module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        textShadow: {
+          sm: '0 1px 2px var(--tw-shadow-color)',
+          DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+          lg: '0 8px 16px var(--tw-shadow-color)',
         },
-        { values: theme('textShadow') }
-      )
-    }),
-  ],
-};
+        dropShadow: {
+          custom: '0 5px 5px var(--tw-shadow-color)'
+        },
+        animation: {
+          fadeOut: 'fadeOut 2s ease-in-out',
+          fadeIn: 'fadeIn 2s ease-in-out',
+        },
+        keyframes: theme => ({
+          fadeOut: {
+            '0%': { opacity: theme('opacity.0') },
+            '100%': { opacity: theme('opacity.100') },
+          },
+          fadeIn: {
+            '100%': { opacity: theme('opactity.100') },
+            '0%': { opacity: theme('opacity.0') },
+          },
+        }),
+      },
+    },
+    plugins: [require('@tailwindcss/typography'),
+      (function ({ matchUtilities, theme }) {
+        matchUtilities(
+          {
+            'text-shadow': (value) => ({
+              textShadow: value,
+            }),
+          },
+          { values: theme('textShadow') }
+        )
+      }),
+    ],
+  };
